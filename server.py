39333b69ecce7d63b7b6d0328350dc2a448d0778
Kevie <65157770+Kevsay@users.noreply.github.com>
2022-11-18 11:29:21 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,24 +1,26 @@
+
 # Socket
-import socket # import socket library
-client_ip = "" # Raspberry PI ip address
-client_port = 13000 # Socket port
+import socket
+server_ip = "" # Ip address of Raspberry PI
+server_port = 13000 # The socket port
 server = socket.socket() # Create socket server
-server.connect((client_ip, client_port)) # Connect socket server to client
+server.bind((server_ip, server_port)) # Bind socket server to ip address and port
+server.listen(5)
+print("Server Online")
+print(server_ip)
 
-# Website
-from flask import Flask, render_template, Response # Import flask library for html
-server_ip = socket.gethostbyname(socket.gethostname()) # Gets server IP address
-server_port = "1011" # Servers Web Port
-app = Flask(__name__) # Creates Flask Application
+# Raspberry PI
+import RPi.GPIO as GPIO
+RPI_Port = 18 # Sets Raspberry PI port
+GPIO.setmode(GPIO.BCM) # Sets GPIO mode
+GPIO.setup(RPI_Port, GPIO.OUT) # Setup Raspberry PI Port
 
+while True: # Loop
+    client, addr = server.accept() # Get client on connection
+    data = client.recv(1024).decode() or None # Gets and decode data
+    client.close() # Close client connection
 
-@app.route("/") # Sets "http://{server_ip}:{server_port}/" to run main function
-def main():
-    return render_template("index.html") # renders index.html
-
-@app.route("/api/<state>", methods = ['POST']) # Listens for POST requests
-def on(state):
-    server.send(state.encode()) # Sends encoded <state> to client
-    return Response(status=200) # Returns status 200 to website
-
-app.run(server_ip, server_port, debug=True) # Starts website
+    if data == "on": # Checks if data equals "on"
+        GPIO.output(RPI_Port, GPIO.HIGH)
+    elif data == "off": # Checks if data equals "off"
+        GPIO.output(RPI_Port, GPIO.LOW)
